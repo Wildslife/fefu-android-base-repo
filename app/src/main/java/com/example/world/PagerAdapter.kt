893af4e9486ee6b02cce2c61309d21332d680e28
Wildslife <95517284+Wildslife@users.noreply.github.com>
2022-01-27 16:28:42 +1000

    Add files via upload

---
+++ app/src/main/java/com/example/world/PagerAdapter.kt
@@ -0,0 +1,18 @@
+package com.example.world
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class PagerAdapter(fragmentActivityFragment: ActivityFragment): FragmentStateAdapter(fragmentActivityFragment) {
+
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position){
+            0 -> MyActivityFragment()
+            else -> UsersActivityFragment()
+        }
+    }
+}
